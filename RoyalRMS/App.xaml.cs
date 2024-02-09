1d8205b625f62c6cf8bf84ed450cb039ad7adafa
Parth Gandhi <A00267045@mycambrian.ca>
2024-02-08 23:25:33 -0500

    Add project files.

---
+++ RoyalRMS/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace RoyalRMS
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
